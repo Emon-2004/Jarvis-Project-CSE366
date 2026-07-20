92d8bbca0ef76967cb63bf472f5268d6aa3f9fd4
Emon-2004 <s0172401848@gmail.com>
2026-07-20 20:22:04 +0600

    Brought the micrphone souce in try case

--- JVA.py
+++ JVA.py
@@ -16,17 +16,19 @@ if __name__ == "__main__":
         # Listen for the wake word "Jarvis"
         # obtain audio from the microphone
         r = sr.Recognizer()
-        with  sr.Microphone() as source:
-            print("Listening...")
-            audio = r.listen(source)
+        
 
         
         print("Recognizing...")
         # recognize speech using Google Speech Recognition
         try:
+            with  sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source,timeout=2,phrase_time_limit = 2)
+                        
             command = r.recognize_google(audio)
             print(command)
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
-        except sr.RequestError as e:
+        except sr.RequestError as e:  #Just In case google speech api or internet is down
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
