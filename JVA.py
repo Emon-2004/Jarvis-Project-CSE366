840f5a0dad7b24751881c866567e5a4bfb6bd0d8
Emon-2004 <s0172401848@gmail.com>
2026-07-22 21:25:53 +0600

    pushing

--- JVA.py
+++ JVA.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import musicLibrary
 
 recognizer = sr.Recognizer()
 
@@ -10,8 +11,26 @@ def speak(text):
     engine.runAndWait()
     engine.stop()
 
-# def processCommand(c):
-#     pass
+def processCommand(c):
+    if "open youtube" in c.lower():
+        speak("Opening YouTube")
+        webbrowser.open("https://www.youtube.com")
+    elif "open google" in c.lower():
+        speak("Opening Google")
+        webbrowser.open("https://www.google.com")
+    elif "open stackoverflow" in c.lower():
+        speak("Opening Stack Overflow")
+        webbrowser.open("https://stackoverflow.com")
+    elif "open github" in c.lower():
+        speak("Opening GitHub")
+        webbrowser.open("https://github.com")
+    elif "open gmail" in c.lower():
+        speak("Opening Gmail")
+        webbrowser.open("https://mail.google.com")
+    elif "open facebook" in c.lower():
+        speak("Opening Facebook")
+        webbrowser.open("https://www.facebook.com")
+
 
 if __name__ == "__main__":
     speak("Initializing Jarvis...")
@@ -38,7 +57,7 @@ if __name__ == "__main__":
                     audio = r.listen(source)      
                     command = r.recognize_google(audio)
 
-                    # processCommand()
+                    processCommand(command)
 
         except Exception as e:  #Just In case google speech api or internet is down
             print("Google Speech Recognition could not understand audio; {0}".format(e))
