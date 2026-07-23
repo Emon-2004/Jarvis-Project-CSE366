cacf5fe7d577bc45cd9e562684183258becd003d
Emon-2004 <s0172401848@gmail.com>
2026-07-23 21:39:50 +0600

    music changed

--- musicLibrary.py
+++ musicLibrary.py
@@ -2,5 +2,5 @@ music = {
     "sunflower" :       "https://youtu.be/ApXoWvfEYVU?si=QUVxGBKMGjBL8afq",
     "dai dai"   :       "https://youtu.be/fcnDmrtj6Sk?si=ThQhiobY8WhGDRZh",
     "blinding lights" : "https://youtu.be/4NRXx6U8ABQ?si=Yic7Q0uyYuDIe6Xq",
-    "KH"            :   "https://youtu.be/YoOd47d_izc?si=5hzUQtdFtAgQqUUI"
+    "kh"            :   "https://youtu.be/YoOd47d_izc?si=5hzUQtdFtAgQqUUI"
 }
